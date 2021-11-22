f8d86ad409128ffe17f1f4c0adb9b1f4d5854b76
Benji41 <noereynoso.business@gmail.com>
2021-11-22 13:34:48 -0700

    Add project files.

---
+++ CalculoAreaCirculo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoAreaCirculo
+{
+    class Program
+    {
+        //algo
+        static void Main(string[] args)
+        {
+            Double area=-1;
+            float radio = -1;
+            //algo
+            Console.WriteLine("Introduzca la longitud del radio:");
+            radio = (float)Convert.ToDouble(Console.ReadLine());
+            area = radio * Math.PI / 2;
+            Console.WriteLine("Area del circulo:"+area);
+            area * *2;
+            Console.ReadLine();
+        }
+    }
+}
